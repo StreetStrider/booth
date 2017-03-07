286e82eb29a89863f5ac2e88269d81f835f3cd2f
Strider <strange.transistor@gmail.com>
2017-03-08 00:17:48 +0300

    +mocha, +chai, test ns-booth.

---
+++ test/case/ns-booth.test.js
@@ -0,0 +1,16 @@
+
+import ns_booth from '../../lib/ns-booth'
+
+import { expect } from 'chai'
+
+describe('ns-booth', () =>
+{
+	it('works', () =>
+	{
+		expect(ns_booth()).eq('@booth/')
+		expect(ns_booth('a')).eq('@booth/a')
+		expect(ns_booth('A')).eq('@booth/A')
+		expect(ns_booth('a/b')).eq('@booth/a/b')
+		expect(ns_booth('a-b')).eq('@booth/a-b')
+	})
+})
