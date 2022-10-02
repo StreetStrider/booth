72ad2b73b35c25838cfa58d6a064294765ec802c
Strider <strange.transistor@gmail.com>
2022-10-02 03:23:50 +0300

    mv utils.

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,7 @@ export { Rooms } from './Booth'
 
 export { default as Booth }    from './Booth'
 export { default as Endpoint } from './Endpoint'
-export         * as Addr       from './Addr'
+export         * as Addr       from './_/Addr'
 
 export { multi as once } from '@streetstrider/emitter/once'
 export { multi as when } from '@streetstrider/emitter/when'
