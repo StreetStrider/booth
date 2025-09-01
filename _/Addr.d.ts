ec949f44b04cbcd0a78d45b194a60cb57c5ed6d9
Strider <strange.transistor@gmail.com>
2025-09-01 03:08:38 +0300

    formalize addr type.

--- _/Addr.d.ts
+++ _/Addr.d.ts
@@ -1,30 +1,30 @@
 
-export function Websocket <Port extends number> (port: Port): Websocket<Port>
+import type { ServerOptions } from 'ws'
 
-export type Websocket <Port extends number> =
+
+export interface Addr
 {
-	readonly port: Port;
-	for_dispatch (): { port: Port };
-	for_endpoint (host?: string): string;
-	view ():
-	readonly
-	[
-		ReturnType<Websocket<Port>['for_dispatch']>,
-		ReturnType<Websocket<Port>['for_endpoint']>,
-	];
-}
-
-export function Unix <Path extends string> (path: Path): Unix<Path>
-
-export type Unix <Path extends string> =
-{
-	readonly path: Path;
-	for_dispatch (): { path: Path };
+	for_dispatch (): ServerOptions;
 	for_endpoint (): string;
 	view ():
 	readonly
 	[
-		ReturnType<Unix<Path>['for_dispatch']>,
-		ReturnType<Unix<Path>['for_endpoint']>,
+		ReturnType<this['for_dispatch']>,
+		ReturnType<this['for_endpoint']>,
 	];
+}
+
+
+export  function Websocket <Port extends number> (port: Port): Websocket<Port>
+export interface Websocket <Port extends number> extends Addr
+{
+	readonly port: Port;
+	for_dispatch (): { port: Port };
+}
+
+export  function Unix <Path extends string> (path: Path): Unix<Path>
+export interface Unix <Path extends string> extends Addr
+{
+	readonly path: Path;
+	for_dispatch (): { path: Path };
 }
